e783b9650ef240434457117d1e46c30d6caaef5d
maximburkov <maxburkov12@gmail.com>
2019-08-06 00:22:45 +0300

    added messaging, button after loading

--- background.js
+++ background.js
@@ -1,25 +1,47 @@
-console.log('background script');
 const networkFilters = {
     urls: [
-        //"https://*.storage.yandex.net/*"
-        "<all_urls>"
+        "https://*.storage.yandex.net/*"
     ]
 };
+
+//replace by guid
+var id = 0;
+//not very good guid implementation
+// function uuid(){
+//     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,function(c,r){
+//         return 'x'==с?(r=Math.random()*16|0):(r&0x3|0x8).toString(16)
+//     });
+// }
+
+//cash for audio files
+var cash = [];
 
 chrome.webRequest.onCompleted.addListener((details) => {
     console.log('got request');
     if(details.type == 'media'){
-        console.log('it is media');        
-        console.log(details);
 
-        chrome.downloads.download(
-            {url: details.url,
-             saveAs: true
-            },
-            function(res){
-                console.log('loaded.')
-                console.log(res);                
+        var song = {
+            id: ++id,
+            url: details.url
+        };
+
+        console.log(song);     
+        cash.push(song);
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {url: song.url, id: song.id}, function(response) {
+              console.log(response.message);
             });
+          });
+
+        // chrome.downloads.download(
+        //     {url: details.url,
+        //      saveAs: true
+        //     },
+        //     function(res){
+        //         console.log('loaded.')
+        //         console.log(res);                
+        //     });
     }
 
 }, networkFilters);
--- content.js
+++ content.js
@@ -1,4 +1,16 @@
 (function () {
     console.log('In Yandex music');   
+
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+          console.log(sender.tab ?
+                      "from a content script:" + sender.tab.url :
+                      "from the extension");
+
+          $('.player-controls__track-controls').append('<button data-id="'+request.id+'" data-url="'+request.url+'">Load</button>');
+
+          sendResponse({message: "please load"});
+        });
 })();
 
