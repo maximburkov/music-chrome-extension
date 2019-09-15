16d9e40f550862d8a9075ab766130ce23bedc500
maximburkov <maxburkov12@gmail.com>
2019-09-15 15:53:38 +0300

    works with names

--- background.js
+++ background.js
@@ -20,17 +20,18 @@ chrome.webRequest.onCompleted.addListener((details) => {
     console.log('got request');
     if(details.type == 'media'){
 
-        var song = {
-            id: ++id,
-            url: details.url
-        };
+        // var song = {
+        //     id: ++id,
+        //     url: details.url
+        // };
 
-        console.log(song);     
-        cash.push(song);
+        //console.log(song);     
+        //cash.push(song);
+        cash[++id] = details.url;
 
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {url: song.url, id: song.id}, function(response) {
-              console.log(response.message);
+            chrome.tabs.sendMessage(tabs[0].id, {url: details.url, id: id}, function(response) {
+              //console.log(response.message);
             });
           });
 
@@ -45,3 +46,21 @@ chrome.webRequest.onCompleted.addListener((details) => {
     }
 
 }, networkFilters);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.status == "load"){
+        console.log('Start loading: ' + request.id);
+
+        chrome.downloads.download(
+            {url: cash[request.id],
+             saveAs: true,
+             filename: request.artist + ' - ' + request.name
+            },
+            function(res){
+                console.log('loaded.')
+                console.log(res);                
+            });
+      }
+        sendResponse({farewell: "message got"});
+    });
--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 (function () {
     console.log('In Yandex music');   
 
+    function LoadAudio(){
+      console.log('check');
+    }
 
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
@@ -8,9 +11,29 @@
                       "from a content script:" + sender.tab.url :
                       "from the extension");
 
-          $('.player-controls__track-controls').append('<button data-id="'+request.id+'" data-url="'+request.url+'">Load</button>');
+          $('.player-controls__track-controls').append('<button class="load-button" data-id="'+request.id+'" data-url="'+request.url+'">Load</button>');
+          $('[data-id="'+request.id+'"].load-button').click(function(){
+              console.log('click!');
+              console.log($(this));
+
+              var name = $(this).parents('.player-controls__wrapper').find('.track__title').html();
+              var artist = $(this).parents('.player-controls__wrapper').find('.d-artists a').html();
+
+              //send message to the background
+              chrome.runtime.sendMessage({
+                status: "load", 
+                id: request.id,
+                name: name,
+                artist: artist
+              }, function(response) {
+                //console.log(response.);
+              });
+          });
+
 
           sendResponse({message: "please load"});
         });
 })();
+
+
 
